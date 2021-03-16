fabfcbe9b4a772f3389054ac176cb7492e8a0c0e
Tamzeed Rahman <tamzeedrahman@gmail.com>
2021-03-15 23:43:39 -0400

    added text animation

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+//animation
+let typed = new Typed(".typing",{
+    strings:["Developer", "Gamer", "Keycap Artisan", "Mechanical Keyboard Crafter", "Pursuit Fellow", "Hardware Enthusiast"],
+    typeSpeed: 100,
+    backSpeed:60,
+    loop:true
+})
+
+
+
+
+//background color changing
+
 const btn = document.querySelector(".btn-toggle");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
